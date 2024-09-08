eb786506833c96199fd319b119def56381563360
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-08 12:29:01 +0530

    first java program

---
+++ demo1.java
@@ -0,0 +1,13 @@
+public class demo1
+{
+    public static void main(String[] args)
+    {
+        demo1 d = new demo1();
+        d.m1();
+
+    }
+    public void m1()
+    {
+        System.out.println("Hello World");
+    }
+}
