94ccf292d76c93cad08724028e3dd54d33db6c3d
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-09 19:03:59 +0530

    My name Program

---
+++ MyName.java
@@ -0,0 +1,14 @@
+public class MyName 
+{
+    public static void main(String[] args)
+    {
+        MyName m = new MyName();
+        m.name();
+    }
+    public void name()
+    {
+        System.out.println("Raghuveer");
+    }
+    
+}
+
