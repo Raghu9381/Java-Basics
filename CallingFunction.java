52e5b25e366245f9ab8e547de73ceed344b0e280
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-09 19:31:48 +0530

    Calling a Function from Main

---
+++ CallingFunction.java
@@ -0,0 +1,13 @@
+public class CallingFunction 
+{
+    public static void function()
+    {
+        System.out.println("Function is Called");
+    }
+
+    public static void main(String[] args) 
+    {
+        function();        
+    }
+    
+}
