4f4503bf7cb76a9fbd808625dbdc450df3b3fca0
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-09 19:15:47 +0530

    Comments Program

---
+++ Comments.java
@@ -0,0 +1,22 @@
+public class Comments 
+{
+    //It is a Single Line Commen.
+
+    /*It is a multiple line comment
+     * we can write all the information here so that 
+     * the user will understand easily.
+     */
+
+    /**
+       Documentation comment
+
+       Example:
+       Name of the Project : Online Shopping
+       Date created :- 12-12-2023
+       Last Modified - 16-01-2024
+       Author :- Raghuveer
+       Modules : -  10 Modules
+     */
+
+}
+
