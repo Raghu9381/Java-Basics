dca1d7c2eef9eab43544542f722d1c955706c44f
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-09 19:21:55 +0530

    Data Type Examples Program

---
+++ DataTypesExample.java
@@ -0,0 +1,20 @@
+public class DataTypesExample
+ {
+    public static void main(String[] args) 
+    {
+        // Declare variables of different data types
+        int myInt = 25;
+        boolean myBoolean = true;
+        char myChar = 'R';
+        float myFloat = 12.34f;
+        double myDouble = 23.456;
+
+        // Printing the variables on the console
+        System.out.println("Integer: " + myInt);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("Character: " + myChar);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+    }
+}
+
