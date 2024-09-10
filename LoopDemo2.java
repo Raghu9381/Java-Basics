cb12b414c2bed8db1a95bbcf58da6b03ccbb831f
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-10 21:09:08 +0530

    Printing Numbers using Loop

---
+++ LoopDemo2.java
@@ -0,0 +1,13 @@
+public class LoopDemo2
+{
+    public static void main(String[] args) 
+    {
+        for(int i=1;i<=20;i++)
+        {
+            System.out.print(i+ " ");
+        }
+        
+    }
+    
+}
+
