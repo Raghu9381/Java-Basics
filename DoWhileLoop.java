fd786998a6508bf8b401a6cf9bf0a51d9dfdf2b8
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-10 21:52:27 +0530

    Using Do-While loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,17 @@
+public class DoWhileLoop 
+{
+    public static void main(String[] args) 
+    {
+        int i=1;
+        System.out.println("printing numbers using do-while loop");
+        do
+        {
+            System.out.print(i+" ");
+            i++;
+
+        }
+        while(i<=10);
+    }
+    
+}
+
