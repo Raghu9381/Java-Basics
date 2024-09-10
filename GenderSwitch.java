9492bd9518522b97e034a9075bf36c515140f5bf
Raghu9381 <nagularaghu7604@gmail.com>
2024-09-10 22:41:15 +0530

    Gender Switch program using Switch

---
+++ GenderSwitch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GenderSwitch {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter M for Male or F for Female: ");
+        char gender = sc.next().charAt(0);
+
+        switch (gender)
+         {
+            case 'M':
+            case 'm':
+                System.out.println("Male");
+                break;
+            case 'F':
+            case 'f':
+                System.out.println("Female");
+                break;
+            default:
+                System.out.println("Invalid input. Please enter M or F.");
+              
+                break;
+        }
+        sc.close();
+    }
+}
+
